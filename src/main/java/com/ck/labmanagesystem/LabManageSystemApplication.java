0966c71dd6d7c0b528bb22bb749153cfd3c51607
ybht <1038211234@qq.com>
2025-10-18 20:39:29 +0800

    高校实验室管理系统demo

---
+++ src/main/java/com/ck/labmanagesystem/LabManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ck.labmanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabManageSystemApplication.class, args);
+    }
+
+}
